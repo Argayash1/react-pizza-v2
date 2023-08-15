c67021dc86cdae3ddb4ab701c77a1dffdba31f3d
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-08-16 02:08:54 +0500

    feat: разделил код на компоненты

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello, World!</h1>
+    <div class='wrapper'>
+      <Header />
+      <div class='content'>
+        <div class='container'>
+          <div class='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class='content__title'>Все пиццы</h2>
+          <div class='content__items'>
+            <PizzaBlock title='Чизбургер-пицца' price={395} />
+            <PizzaBlock title='Пицца карбонара' price={500} />
+            <PizzaBlock title='Пицца Маргарита' price={495} />
+            <PizzaBlock title='Пицца Четыре сыра' price={550} />
+            <PizzaBlock title='Пицца Сальмоне' price={600} />
+            <PizzaBlock title='Пицца Болоньезе' price={595} />
+            <PizzaBlock title='Пицца со страчателлой' price={450} />
+            <PizzaBlock title='Пицца мясная' price={560} />
+            <PizzaBlock title='Пицца с салями' price={400} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div class='categories'>
+      <ul>
+        <li class='active'>Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
