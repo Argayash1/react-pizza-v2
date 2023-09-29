dced6b40d981358b14ff5a6b90b758a3ecf04eff
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-09-29 21:37:47 +0500

    feat: сделал, чтобы кнопка уменьшения товара дизейблилась, когда количество товаров=1

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -44,7 +44,11 @@ const CartItemBlock: React.FC<CartItemProps> = ({ id, title, type, size, price, 
         </p>
       </div>
       <div className='cart__item-count'>
-        <div onClick={onClickMinus} className='button button--outline button--circle cart__item-count-minus'>
+        <button
+          disabled={count === 1}
+          onClick={onClickMinus}
+          className='button button--outline button--circle cart__item-count-minus'
+        >
           <svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
               d='M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z'
@@ -55,9 +59,9 @@ const CartItemBlock: React.FC<CartItemProps> = ({ id, title, type, size, price, 
               fill='#EB5A1E'
             />
           </svg>
-        </div>
+        </button>
         <b>{count}</b>
-        <div onClick={onClickPlus} className='button button--outline button--circle cart__item-count-plus'>
+        <button onClick={onClickPlus} className='button button--outline button--circle cart__item-count-plus'>
           <svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
               d='M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z'
@@ -68,7 +72,7 @@ const CartItemBlock: React.FC<CartItemProps> = ({ id, title, type, size, price, 
               fill='#EB5A1E'
             />
           </svg>
-        </div>
+        </button>
       </div>
       <div className='cart__item-price'>
         <b>{price * count} ₽</b>
