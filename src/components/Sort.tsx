d43c49e07e7fbc1324f620ceca83423b3264c961
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-10-02 00:47:14 +0500

    fix: сделал, чтобы значения в попапе сортировки не переносились

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -13,12 +13,12 @@ type PopupClick = MouseEvent & {
 };
 
 export const sortList: SortItem[] = [
-  { name: 'популярности (DESC)', sortProperty: SortPropertyEnum.RATING_DESC },
-  { name: 'популярности (ASC)', sortProperty: SortPropertyEnum.RATING_ASC },
-  { name: 'цене (DESC)', sortProperty: SortPropertyEnum.PRICE_DESC },
-  { name: 'цене (ASC)', sortProperty: SortPropertyEnum.PRICE_ASC },
-  { name: 'алфавиту (DESC)', sortProperty: SortPropertyEnum.TITLE_DESC },
-  { name: 'алфавиту (ASC)', sortProperty: SortPropertyEnum.TITLE_ASC },
+  { name: 'популярности  ↓', sortProperty: SortPropertyEnum.RATING_DESC },
+  { name: 'популярности ↑', sortProperty: SortPropertyEnum.RATING_ASC },
+  { name: 'цене  ↓', sortProperty: SortPropertyEnum.PRICE_DESC },
+  { name: 'цене ↑', sortProperty: SortPropertyEnum.PRICE_ASC },
+  { name: 'алфавиту  ↓', sortProperty: SortPropertyEnum.TITLE_DESC },
+  { name: 'алфавиту ↑', sortProperty: SortPropertyEnum.TITLE_ASC },
 ];
 
 type SortPopupProps = {
